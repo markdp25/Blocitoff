f81958ce019c8051a5bbdba732b985f33743cb1d
Mark Dellaposta <markdellaposta@Marks-MacBook-Pro.local>
2017-12-05 21:38:10 -0500

    Sign in and out

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,20 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'users/index'
+
+  get 'users/edit'
+
+  get 'users/show'
+
+  devise_for :users
+
   get 'welcome/index'
 
   get 'welcome/about'
 
   root 'welcome#index'
-  
-  devise_for :users
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
