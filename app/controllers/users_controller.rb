030e0b2cdaaa218f3c09d3508844f8aa1d79d5b8
Mark Dellaposta <markdellaposta@Marks-MacBook-Pro.local>
2017-12-08 18:42:32 -0500

    Created basic to do list capabilities

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,12 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(current_user.id)
+    if !current_user.nil?
+      @user = current_user
+      @item = Item.new
+      @items = @user.items
+    else
+      redirect_to new_user_registration_path
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  get 'items/create'
+
+  devise_for :users
+
+  resources :users do
+    resources :items, only: [:new, :create]
+  end
+
+  resources :items, only: [:new, :create]
+
+
   get 'users/new'
 
   get 'users/index'
@@ -6,8 +17,6 @@ Rails.application.routes.draw do
   get 'users/edit'
 
   get 'users/show'
-
-  devise_for :users
 
   get 'welcome/index'
 
